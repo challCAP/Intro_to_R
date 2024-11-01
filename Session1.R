36ad49fddc233bcf5c3e967a85e9f52d3b82b0d2
cgwhall <102544681+cgwhall@users.noreply.github.com>
2024-11-01 12:29:39 -0400

    First commit of session 1 script
    Session 1 R Script

---
+++ Session1.R
@@ -0,0 +1,44 @@
+##  Intro to Data Analysis in R ##
+##  Session 1
+##  Data Types, Data Structures, and R Syntax
+##  by Chandler Hall
+##
+##  11/1/24
+
+
+#######################################################
+# Welcome to world of coding in R for Data Analysis!
+######################################################
+
+## Using R Basics
+
+# What you're reading right now is a COMMENT, this is text that your interpreter will ignore when running a section of code
+# COMMENTS are used to improve code readilibility and help organize sections of your code. use the '#' at the beginning or end of a line to
+# start a COMMENT, such as the one at the beginning of this line,
+# and this one.
+# and this one.
+
+#####
+# Notice, if I use four '#' symbols to start a line, like I did just above, a carrot appears to left. Click on it and see what happens!
+# Everything below it until the next section is hidden. Remember this as you start to write longer scripts to help with organization!
+#####
+
+# Now let's talk about actual code. Put your cursor to end of the next line and on your keyboard press CMD+Return (CTRL+Enter for PC)
+print('Hello World')
+
+# You should see "Hello World" was printed in the console below.
+# Now lets see seem something slightly more interesting. Run the next line of code.
+2+2
+# Truly, we live in the future.
+
+# You can also run multiple lines of code at the same time. Highlight the 4 lines of code below and press CMD+Return (CTRL+Enter for PC).
+# Notice that only the print statement gives and output in your console, but the other lines of code ran.
+x <- 8
+y <- 10
+answer <- x*y
+print(answer)
+
+
+
+
+
